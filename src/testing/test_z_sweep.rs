41d5b2ee40348a5ba1bc768de433849bd4d4a62c
Silas <kammersi.to@gmail.com>
2026-07-04 00:44:27 +0200

    Fix Frustum Culling loose OBB false positives with hierarchical sub-OBBs and horizon checks

---
+++ src/testing/test_z_sweep.rs
@@ -0,0 +1,88 @@
+use super::test_frustum_coverage::{dvec3_to_lat_lon, tile_contains, intersect_ellipsoid};
+use crate::camera::camera::Camera;
+use glam::{Vec3, Vec4};
+use crate::globe::quadtree::{TileId, QuadtreeManager};
+use rayon::prelude::*;
+use std::io::Write;
+
+fn evaluate_direct_camera(file: &mut std::fs::File, z: f32) {
+    let pos = Vec3::new(0.0, 0.0, z);
+    let mut cam = Camera::new(pos, Vec3::ZERO);
+    cam.set_eye(pos, Vec3::ZERO);
+    
+    let w = 1920;
+    let h = 1080;
+
+    let mut quadtree = QuadtreeManager::new();
+    let view_proj = cam.get_projection_matrix(w as f32 / h as f32) * cam.get_view_matrix();
+    for _ in 0..30 {
+        quadtree.update(cam.global_transform().0, view_proj);
+    }
+    let visible_tiles_data = quadtree.get_visible_tiles();
+    let visible_tiles: Vec<TileId> = visible_tiles_data.iter().map(|(id, _, _)| *id).collect();
+    let rendered_tiles = visible_tiles.len();
+    
+    if rendered_tiles == 0 {
+        writeln!(file, "{},0,0,0,0", z).unwrap();
+        return;
+    }
+    
+    let step_x = (w / 100).max(1);
+    let step_y = (h / 80).max(1);
+    
+    let mut points = Vec::new();
+    for x in (0..w).step_by(step_x as usize) {
+        for y in (0..h).step_by(step_y as usize) {
+            points.push((x, y));
+        }
+    }
+    
+    let results: Vec<Result<TileId, ()>> = points.par_iter().filter_map(|&(x, y)| {
+        let (ray_origin, ray_dir) = cam.screen_to_world_ray(x as f32, y as f32, w as f32, h as f32);
+        if let Some(hit) = intersect_ellipsoid(ray_origin, ray_dir) {
+            let view_pos = cam.get_view_matrix() * Vec4::new(hit.x as f32, hit.y as f32, hit.z as f32, 1.0);
+            if view_pos.z > 0.0 { return None; }
+            let (hit_lat, hit_lon) = dvec3_to_lat_lon(hit);
+            for tile in &visible_tiles {
+                if tile_contains(tile, hit_lat, hit_lon) {
+                    return Some(Ok(*tile));
+                }
+            }
+            return Some(Err(()));
+        }
+        None
+    }).collect();
+    
+    let mut hit_set = std::collections::HashSet::new();
+    let mut false_negatives = 0;
+    
+    for res in results {
+        match res {
+            Ok(tile) => { hit_set.insert(tile); },
+            Err(_) => { false_negatives += 1; },
+        }
+    }
+    
+    let hit_tiles = hit_set.len();
+    let false_positives = rendered_tiles.saturating_sub(hit_tiles);
+    
+    writeln!(file, "{},{},{},{},{}", z, rendered_tiles, hit_tiles, false_positives, false_negatives).unwrap();
+}
+
+#[test]
+fn test_z_sweep() {
+    let mut file = std::fs::File::create("z_sweep_results_fine.csv").unwrap();
+    writeln!(file, "z,rendered,hit,false_positives,false_negatives").unwrap();
+    
+    println!("Running Z-Axis Sweep (Pos=0,0,Z)...");
+    
+    let min_z = 8.0;
+    let max_z = 8.5;
+    let num_steps = 100;
+    
+    for i in 0..=num_steps {
+        let t = i as f32 / num_steps as f32;
+        let z = min_z + (max_z - min_z) * t;
+        evaluate_direct_camera(&mut file, z);
+    }
+}
