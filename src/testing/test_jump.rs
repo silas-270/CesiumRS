c15be643afab9b468ab15323d8ba5f279efa7185
Silas <kammersi.to@gmail.com>
2026-07-08 00:36:26 +0200

    Clean up unused test files and images

--- src/testing/test_jump.rs
+++
@@ -1,5 +0,0 @@
-use crate::flight::app::FlightApp;
-#[test]
-fn test_get_plane_state_jump() {
-    // we can't easily instantiate FlightApp in a unit test, let's just inspect the trajectory evaluator.
-}
