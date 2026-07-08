fb1c3bfb44705c1e24597fc465c6086bf48b646f
Silas <kammersi.to@gmail.com>
2026-07-08 09:12:38 +0200

    Restrict engine internal visibility using a testing feature flag

--- crates/cesium-engine/src/render/mod.rs
+++ crates/cesium-engine/src/render/mod.rs
@@ -1,8 +1,20 @@
+#[cfg(feature = "testing")]
 pub mod wgpu_state;
+#[cfg(not(feature = "testing"))]
+pub(crate) mod wgpu_state;
+
 pub mod capture;
 pub mod debug_geometry;
+
+#[cfg(feature = "testing")]
 pub mod tile_display;
+#[cfg(not(feature = "testing"))]
+pub(crate) mod tile_display;
+
+#[cfg(feature = "testing")]
 pub mod camera_uniform;
+#[cfg(not(feature = "testing"))]
+pub(crate) mod camera_uniform;
 pub mod globe_pipeline;
 pub mod sky_pipeline;
 pub mod model_pipeline;
