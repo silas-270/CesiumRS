0ab75de0997a0cb31ff11fb80e21a7f97a0ea8ff
Silas <kammersi.to@gmail.com>
2026-07-08 09:12:38 +0200

    Restrict engine internal visibility using a testing feature flag

--- crates/cesium-engine/src/globe/mod.rs
+++ crates/cesium-engine/src/globe/mod.rs
@@ -1,4 +1,12 @@
 pub mod geometry;
+#[cfg(feature = "testing")]
 pub mod quadtree;
+#[cfg(not(feature = "testing"))]
+pub(crate) mod quadtree;
+
 pub mod terrain_parser;
+
+#[cfg(feature = "testing")]
 pub mod tiles;
+#[cfg(not(feature = "testing"))]
+pub(crate) mod tiles;
